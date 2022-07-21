8847d7c15a0b0da0ad34b50c9eb2ac6b5169c994
juacomiranda <jmiranda@live.cl>
2022-07-21 01:24:29 +0000

    domain total

--- src/app.js
+++ src/app.js
@@ -8,4 +8,19 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+  // .com, .net, .us, .io, etc.
+  var ext = [".com", ".net", ".us", ".io"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let p = 0; p < noun.length; p++) {
+        for (let f = 0; f < ext.length; f++)
+          console.log(pronoun[i] + adj[j] + noun[p] + ext[f]);
+      }
+    }
+  }
 };
